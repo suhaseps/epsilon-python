34e94f9f686e51f812a2bbee08b913ca3fa9613e
root <root@ip-172-31-16-178.ap-northeast-2.compute.internal>
2018-11-12 09:33:00 +0000

    adding rev.py

---
+++ pythonscripts/rev.py
@@ -0,0 +1,19 @@
+s = input("whats your name: ")
+s = int(s)
+print(type(s))
+
+#print (s[::-1])
+#s=s[::-1]
+#print(s)
+
+#s[4] = "l"
+
+#z=s.replace("s","S",1)
+#print(z)
+#print(s.count("S"))
+#print (help(s.replace))
+
+#print(s.rjust(25,"#"))
+#print(s.ljust(25,"#"))
+
+
